1efb9063d9719b6d3027eaee5e8ed57cf4dc9d44
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-03-10 14:48:38 +0200

    Init commit
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

---
+++ mobile/src/main/java/il/co/wearabledevices/mudramediaplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package il.co.wearabledevices.mudramediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
