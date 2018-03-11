72ede61d599efcf7ca4dc036291b028dff446d15
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-03-11 23:45:24 +0200

    COnfigured Gradle versions
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- mobile/src/main/java/il/co/wearabledevices/mudramediaplayer/MainActivity.java
+++ mobile/src/main/java/il/co/wearabledevices/mudramediaplayer/MainActivity.java
@@ -1,13 +1,18 @@
 package il.co.wearabledevices.mudramediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBrowseAlbums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBrowseAlbums = findViewById(R.id.bt_sel_albm);
+
     }
 }
