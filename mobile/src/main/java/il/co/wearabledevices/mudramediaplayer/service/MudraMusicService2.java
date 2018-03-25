86652d1ff9f27f659bb43bf48cf9ac93add8da37
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-03-25 20:11:28 +0300

    Saving progress
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- mobile/src/main/java/il/co/wearabledevices/mudramediaplayer/service/MudraMusicService2.java
+++ mobile/src/main/java/il/co/wearabledevices/mudramediaplayer/service/MudraMusicService2.java
@@ -5,21 +5,31 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v4.media.MediaBrowserServiceCompat;
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaButtonReceiver;
 import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.app.NotificationCompat;
 import android.text.TextUtils;
 
+import java.io.IOException;
 import java.util.List;
 
 import il.co.wearabledevices.mudramediaplayer.R;
+import il.co.wearabledevices.mudramediaplayer.model.Playlist;
 
 /**
  * Created by tegra on 25/03/18.
@@ -29,6 +39,7 @@ public class MudraMusicService2 extends MediaBrowserServiceCompat implements Med
     private static final String TAG = MudraMusicService2.class.getSimpleName();
     private static final float LOW_VOLUME = 0.3f;
     private static final float FULL_VOLUME = 1.0f;
+    private Playlist nowPlaying;
     private MediaPlayer mMediaPlayer;
     private MediaSessionCompat mMediaSessionCompat;
 
@@ -42,6 +53,58 @@ public class MudraMusicService2 extends MediaBrowserServiceCompat implements Med
     };
 
     private MediaSessionCompat.Callback mMediaSessionCallback = new MediaSessionCompat.Callback() {
+        @Override
+        public void onCommand(String command, Bundle extras, ResultReceiver cb) {
+            super.onCommand(command, extras, cb);
+            if ("COMMAND_EXAMPLE".equalsIgnoreCase(command)) {
+                //Custom command here
+            }
+        }
+
+        @Override
+        public void onPlayFromUri(Uri uri, Bundle extras) {
+            super.onPlayFromUri(uri, extras);
+        }
+
+        @Override
+        public void onSkipToNext() {
+            super.onSkipToNext();
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            super.onSkipToPrevious();
+        }
+
+        @Override
+        public void onFastForward() {
+            super.onFastForward();
+        }
+
+        @Override
+        public void onRewind() {
+            super.onRewind();
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+        }
+
+        @Override
+        public void onSeekTo(long pos) {
+            super.onSeekTo(pos);
+        }
+
+        @Override
+        public void onSetRepeatMode(int repeatMode) {
+            super.onSetRepeatMode(repeatMode);
+        }
+
+        @Override
+        public void onSetShuffleMode(int shuffleMode) {
+            super.onSetShuffleMode(shuffleMode);
+        }
 
         @Override
         public void onPlay() {
@@ -49,21 +112,113 @@ public class MudraMusicService2 extends MediaBrowserServiceCompat implements Med
             if (!successfullyRetrievedAudioFocus()) {
                 return;
             }
+            mMediaSessionCompat.setActive(true);
+            setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING);
+            showPlayingNotification();
+            mMediaPlayer.start();
         }
 
         @Override
         public void onPause() {
             super.onPause();
+
+            if (mMediaPlayer.isPlaying()) {
+                mMediaPlayer.pause();
+                setMediaPlaybackState(PlaybackStateCompat.STATE_PAUSED);
+                showPausedNotification();
+            }
         }
 
         @Override
         public void onPlayFromMediaId(String mediaId, Bundle extras) {
             super.onPlayFromMediaId(mediaId, extras);
+            try {
+                AssetFileDescriptor afd = getResources().openRawResourceFd(Integer.valueOf(mediaId));
+                if (afd == null) {
+                    return;
+                }
+
+                try {
+                    mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+
+                } catch (IllegalStateException e) {
+                    mMediaPlayer.release();
+                    initMediaPlayer();
+                    mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                }
+
+                afd.close();
+                initMediaSessionMetadata();
+
+            } catch (IOException e) {
+                return;
+            }
+
+            try {
+                mMediaPlayer.prepare();
+            } catch (IOException e) {
+            }
+
+            /* From here on should implement what is in the Bundle class */
+            // Bundle is simply a Map<String>
         }
+
+
     };
 
     public MudraMusicService2() {
         super();
+    }
+
+    private void showPlayingNotification() {
+        NotificationCompat.Builder builder = MediaStyleHelper.from(MudraMusicService2.this, mMediaSessionCompat);
+        if (builder == null) {
+            return;
+        }
+
+        builder.addAction(new NotificationCompat.Action(android.R.drawable.ic_media_pause, "Pause", MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE)));
+        builder.setStyle(new NotificationCompat.MediaStyle().setShowActionsInCompactView(0).setMediaSession(mMediaSessionCompat.getSessionToken()));
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat.from(MudraMusicService2.this).notify(1, builder.build());
+    }
+
+    private void showPausedNotification() {
+        NotificationCompat.Builder builder = MediaStyleHelper.from(this, mMediaSessionCompat);
+        if (builder == null) {
+            return;
+        }
+
+        builder.addAction(new NotificationCompat.Action(android.R.drawable.ic_media_play, "Play", MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE)));
+        builder.setStyle(new NotificationCompat.MediaStyle().setShowActionsInCompactView(0).setMediaSession(mMediaSessionCompat.getSessionToken()));
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat.from(this).notify(1, builder.build());
+    }
+
+    private void initMediaSessionMetadata() {
+        MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
+//Notification icon in card
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+
+//lock screen icon for pre lollipop
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, "Display Title");
+        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, "Display Subtitle");
+        metadataBuilder.putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, 1);
+        metadataBuilder.putLong(MediaMetadataCompat.METADATA_KEY_NUM_TRACKS, 1);
+
+        mMediaSessionCompat.setMetadata(metadataBuilder.build());
+    }
+
+    private void setMediaPlaybackState(int state) {
+        PlaybackStateCompat.Builder PSB = new PlaybackStateCompat.Builder();
+        if (state == PlaybackStateCompat.STATE_PLAYING) {
+            PSB.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PAUSE);
+        } else {
+            PSB.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PLAY);
+        }
+        PSB.setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 0);
+        mMediaSessionCompat.setPlaybackState(PSB.build());
     }
 
     private boolean successfullyRetrievedAudioFocus() {
@@ -130,7 +285,7 @@ public class MudraMusicService2 extends MediaBrowserServiceCompat implements Med
     @Override
     public void onCreate() {
         super.onCreate();
-
+        nowPlaying = nowPlaying = new Playlist();
         initMediaPlayer();
         initMediaSession();
         initNoisyReceiver();
@@ -165,5 +320,22 @@ public class MudraMusicService2 extends MediaBrowserServiceCompat implements Med
                 break;
             }
         }
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.abandonAudioFocus(this);
+        unregisterReceiver(mNoisyReceiver);
+        mMediaSessionCompat.release();
+        NotificationManagerCompat.from(this).cancel(1);
     }
 }
